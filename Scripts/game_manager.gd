530a1b715219691afbb68a6d34e116f098b2bd7c
victorMusta <victorgrabowski33@gmail.com>
2024-05-14 09:37:52 +0200

    Changed level design to be monodirectionnal

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -5,6 +5,6 @@ var score = 0;
 
 func add_score():
 	score +=1
-	score_label.text = "You currently have " + str(score) + " coins!"
-	if(score == 11):
-		score_label.text = "You finished the game! \n Well done!"
+	score_label.text = "You currently have " + str(score) + "/12 coins (noob!)"
+	if(score == 12):
+		score_label.text = "You finished the game! \n Well done!\n Please rest now..."
